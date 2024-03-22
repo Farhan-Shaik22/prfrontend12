bc3f2fe77980704796077207087bba0b6290fef8
Banda-Aakash <aakashbanda03@gmail.com>
2024-03-23 02:26:44 +0530

    final commit 22-3-24

--- src/app/intro.js
+++ src/app/intro.js
@@ -3,59 +3,151 @@ const duction = {
       {
         id: "1",
         image:"/Vachan.jpeg",
-        name: "Vachan",
-        quote: "\" We are hosting some great events , which will make your day memorable, so please join if you like it \"",
+        name: "Vachan 1",
+        quote: "\" Debate: Polish your argumentative skills as you debate your opponent and prove your point to take home a prize.  \"",
 
        
         insta:"https://www.instagram.com/vachan.official/"
       },
-    
+
       {
         id: "2",
-        image:"https://scontent.cdninstagram.com/v/t51.2885-19/258731026_459779118901767_1434310398993580285_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent.cdninstagram.com&_nc_cat=103&_nc_ohc=EuN9XHtBwJkAX_bmxqT&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfAjaUnzrNfiTyGJEJxtpmrbIX8aFgjOri6KDG_GW08GXA&oe=660215D8&_nc_sid=10d13b",
-        name: "Aalap",
-        quote: "\" Aalap is a singers club which is going to conduct events on the day of the event. So we will hope you join!\"",
-        insta:"https://www.instagram.com/aalapofficial/"
+        image:"/Vachan.jpeg",
+        name: "Vachan 2",
+        quote: "\" Declamation: Take command by holding the attention of the audience and moving them with your speech. \"",
+
+       
+        insta:"https://www.instagram.com/vachan.official/"
       },
-    
+
       {
         id: "3",
+        image:"/Vachan.jpeg",
+        name: "Vachan 3",
+        quote: "\" Commentary contest: Test your skill to make scenarios better with quick responses as you commentate and prove your mettle. \"",
+
+       
+        insta:"https://www.instagram.com/vachan.official/"
+      },
+
+
+    
+
+      {
+        id: "4",
+        image:"https://scontent.cdninstagram.com/v/t51.2885-19/258731026_459779118901767_1434310398993580285_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent.cdninstagram.com&_nc_cat=103&_nc_ohc=EuN9XHtBwJkAX_bmxqT&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfAjaUnzrNfiTyGJEJxtpmrbIX8aFgjOri6KDG_GW08GXA&oe=660215D8&_nc_sid=10d13b",
+        name: "Aalap 1",
+        quote: "\" Solo Melodies : Calling all music Enthusiasts!! Here’s your chance to grab the mic and show off your harmonies in this solo singing competition!  \"",
+        insta:"https://www.instagram.com/aalapofficial/"
+      },
+
+      {
+        id: "5",
+        image:"https://scontent.cdninstagram.com/v/t51.2885-19/258731026_459779118901767_1434310398993580285_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent.cdninstagram.com&_nc_cat=103&_nc_ohc=EuN9XHtBwJkAX_bmxqT&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfAjaUnzrNfiTyGJEJxtpmrbIX8aFgjOri6KDG_GW08GXA&oe=660215D8&_nc_sid=10d13b",
+        name: "Aalap 2",
+        quote: "\" Melody Mania!: Get ready for the ultimate battle of bands! Our stage awaits a stellar musical performance by your team. \"",
+        insta:"https://www.instagram.com/aalapofficial/"
+      },
+
+    
+      {
+        id: "6",
         image:"https://scontent.cdninstagram.com/v/t51.2885-19/412046551_359182106802041_6323071453295358439_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent.cdninstagram.com&_nc_cat=105&_nc_ohc=67MBY7Le4D0AX-9bOGD&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfA-3gaThnqQx28ZJ02QPPn6jzdbk9Szav86M_CJuizSbQ&oe=660131FF&_nc_sid=10d13b",
-        name: "Mudra",
-        quote: "\" Mudra is a Dancers club which is going to conduct events on the day of the event. So we will hope you join! \"",
+        name: "Mudra 1",
+        quote: "\" Classical - It’s time to steal the spotlight under the classical hymns and mesmerize the audience with your graceful performance. \"",
         insta:"https://www.instagram.com/mudraofficial/"
       },
 
       {
-        id: "4",
+        id: "7",
+        image:"https://scontent.cdninstagram.com/v/t51.2885-19/412046551_359182106802041_6323071453295358439_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent.cdninstagram.com&_nc_cat=105&_nc_ohc=67MBY7Le4D0AX-9bOGD&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfA-3gaThnqQx28ZJ02QPPn6jzdbk9Szav86M_CJuizSbQ&oe=660131FF&_nc_sid=10d13b",
+        name: "Mudra 2",
+        quote: "\" Freestyle Competition(Group) - Showcase your best moves in this freestyle dance competition and groove your way to victory! \"",
+        insta:"https://www.instagram.com/mudraofficial/"
+      },
+
+      {
+        id: "8",
+        image:"https://scontent.cdninstagram.com/v/t51.2885-19/412046551_359182106802041_6323071453295358439_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent.cdninstagram.com&_nc_cat=105&_nc_ohc=67MBY7Le4D0AX-9bOGD&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfA-3gaThnqQx28ZJ02QPPn6jzdbk9Szav86M_CJuizSbQ&oe=660131FF&_nc_sid=10d13b",
+        name: "Mudra 3",
+        quote: "\" Freestyle Competition(Solo) - Showcase your best moves in this freestyle dance competition and groove your way to victory! \"",
+        insta:"https://www.instagram.com/mudraofficial/"
+      },
+
+      {
+        id: "9",
         image:"https://scontent.cdninstagram.com/v/t51.2885-19/231585681_831325504186531_5797848488481493219_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent.cdninstagram.com&_nc_cat=107&_nc_ohc=r50hr0lRy0gAX_CDIzC&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfA4GA-z6GF315n4tRp70swD2VAQaxMzoJ7TZJ5uGCbAJw&oe=660195A8&_nc_sid=10d13b",
         name: "Aakarshan",
-        quote: "\"Aakarshan is arta and dance club which is going to conduct events on the day of the event. So we will hope you join! \" ",
+        quote: "\"Spin the wheel: Let your art do the talking in this game of chance and skill by spinning the wheel and creating a masterpiece to secure the price. \" ",
         insta:"https://www.instagram.com/aakarshanofficial/"
       },
+
+
       {
-        id: "5",
+        id: "10",
         image:"https://scontent.cdninstagram.com/v/t51.2885-19/324910135_5956884101001489_5671979558690848789_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent.cdninstagram.com&_nc_cat=109&_nc_ohc=5TnSGkmWrwEAX8oaiNB&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfC0ZObtYShTiApjzth7Rlg0VkGzy8ShonepaKD7kLWF7g&oe=66027CA9&_nc_sid=10d13b",
         name: "Riti",
-        quote: "\" Riti is a Fashion club which is going to conduct events on the day of the event. So we will hope you join! \"",
+        quote: "\" Ludo: Creativity meets fun in a perfect fusion of upcycling, fashion and nostalgia on a life-sized Ludo board. Participate to win exciting cash prizes! \"",
         linkedin: "https://www.linkedin.com/in/dheeraj-koka-692a81291/",
         insta:"https://www.instagram.com/riti._.official/"
       },
+
       {
-        id: "6",
+        id: "11",
         image:"https://scontent.cdninstagram.com/v/t51.2885-19/229604920_1493363987688976_8376050562174167657_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent.cdninstagram.com&_nc_cat=104&_nc_ohc=PBhwDa9zWawAX8cT0zo&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfCICW9rj0KwiRepsp9o7DgkFF3XCn2y5tO3-drfzyizCg&oe=6601A8A3&_nc_sid=10d13b",
-        name: "Recurse",
-        quote: "\" Recurse is the coding club which is going to conduct events on the day of the event. So we will hope you join! \"",
+        name: "Recurse 1",
+        quote: "\" Gameathon: Turn the gears of your brain as you brainstorm ideas with your teammates in the ideathon and nurture your innovative potential/flaunt your presentation skills. \"",
         insta:"https://www.instagram.com/recurse.official/"
       },
+
       {
-        id: "7",
-        image:"https://scontent.cdninstagram.com/v/t51.2885-19/118773909_956372321530516_4299631276751353126_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent.cdninstagram.com&_nc_cat=104&_nc_ohc=OV7jQiRXId0AX-aql-v&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfALTIH2O-KB8eOZVtYwKzrb3OZMgyx80K_16pXG-Z_xag&oe=66015579&_nc_sid=10d13b",
-        name: "Kreeda",
-        quote: "\" Kreeda is the sports club which is going to conduct events on the day of the event. So we will hope you join! \"",
-        insta:"https://www.instagram.com/kreedaofficial/"
+        id: "12",
+        image:"https://scontent.cdninstagram.com/v/t51.2885-19/229604920_1493363987688976_8376050562174167657_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent.cdninstagram.com&_nc_cat=104&_nc_ohc=PBhwDa9zWawAX8cT0zo&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfCICW9rj0KwiRepsp9o7DgkFF3XCn2y5tO3-drfzyizCg&oe=6601A8A3&_nc_sid=10d13b",
+        name: "Recurse 2",
+        quote: "\" KBC: Navigate through OG trivia rounds such as fastest finger first, Audience poll etc as you exhibit your gaming and technical knowledge. \"",
+        insta:"https://www.instagram.com/recurse.official/"
+      },
+
+      {
+        id: "13",
+        image:"",
+        name: "Traces Of Lenses",
+        quote: "\" 1)Capture Quest: Take part in this thrilling photography competition, capturing unique visions through fun challenges and honing your skills! \"",
+        insta:"https://www.instagram.com/tracesoflenses/"
+      },
+
+      {
+        id: "14",
+        image:"",
+        name: "Kaivalya",
+        quote: "\" Yoga Competition: Embrace calmness amid chaos, and exhibit your sense of balance and control over your body through fun yoga asanas.  \"",
+        insta:"https://www.instagram.com/kaivalya_official/"
+      },
+
+      {
+        id: "15",
+        image:"/esports.png",
+        name: "E-Sports 1",
+        quote: "\"  Valorant: pump up your adrenaline as you battle to outsmart other teams using abilities, precision and Maneuvers to come out on top.   \" ",
+        insta:"https://www.instagram.com/kaivalya_official/"
+      },
+
+      {
+        id: "16",
+        image:"/esports.png",
+        name: "E-Sports 2",
+        quote: "\"  FIFA: Dribble through the field as you crush the defenders to score goals and dominate your opponent to be a champion.  \"",
+        insta:"https://www.instagram.com/kaivalya_official/"
+      },
+
+      {
+        id: "17",
+        image:"/esports.png",
+        name: "E-Sports 3",
+        quote: "\"  BGMI: Work with your squad to secure a spot in the leaderboard by healing, flanking, camping and defeating your enemies.  \"",
+        insta:"https://www.instagram.com/kaivalya_official/"
       }
-    
+      
     ]
 
 }
