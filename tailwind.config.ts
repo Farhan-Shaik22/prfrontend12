1569249a6dd3b449c528fef483e2dc96ece2a0c0
vardaanaroracodes <vardaanbhatia.perso@gmail.com>
2024-03-23 00:01:38 +0530

    mob/tab/des

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,11 @@ const config: Config =  withMT({
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      'mob': { 'min': '300px', 'max': '850px' },
+      'des': { 'min': '1001px' },
+      'tab': { 'min': '851px', 'max': '1000px' },
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
