c0d26902b1df421eedb3c41f2cec2d3d47df0add
hydroxy42 <farhan2262003@gmail.com>
2024-03-22 22:10:50 +0530

    navbar-change

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -51,8 +51,8 @@ export default function StickyNavbar() {
   );
 
   return (
-    <div className="-m- max-h-[768px] w-full mx-auto   overflow-scroll ">
-      <Navbar className="sticky top-0 z-10 h-max  max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
+    <div className="h-screen overflow-hidden">
+      <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
         <div className="flex items-center justify-between text-blue-gray-900">
           <Typography
             as="a"
