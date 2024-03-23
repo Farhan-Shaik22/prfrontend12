46b8cdf600af9e877c22b2dd948154b4698caead
vardaanaroracodes <vardaanbhatia.perso@gmail.com>
2024-03-23 23:31:41 +0530

    minor change-instafix-tabnamefix

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -47,8 +47,8 @@ export default function Footer() {
     <div class=" bg-transparent py-4 text-gray-100">
       <div class="container mx-auto px-4">
         <div class="-mx-4 flex flex-wrap justify-between">
-          <div class="px-4 w-full font-pixel text-center sm:w-auto sm:text-left">
-           Designed and Developed by Vardaan Bhatia , Shaik Farhan , Aakash Banda
+          <div class="px-4 w-full font-pixel text-center font-semibold sm:w-auto sm:text-left">
+           Designed and Developed by Vardaan Bhatia , Shaik Farhan and Aakash Banda
           </div>
           <div class="px-4 w-full text-center font-pixel sm:w-auto sm:text-left">
             Made with ❤️ by PR KMIT
