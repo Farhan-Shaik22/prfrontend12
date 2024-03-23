9f48d05a8c496567f2d274d4eb649694b3f68853
Banda-Aakash <aakashbanda03@gmail.com>
2024-03-24 00:41:32 +0530

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
         <head>
-         <link rel="icon" href="/Prfavicon.ico" type="image/x-icon" />
+         <link rel="icon" href="/PRlogo.png" type="image/x-icon" />
        </head>
       <body className={inter.className}>
       <Navbar />
