8a9fad800416cd566e0377eaeeee8dec041cdb1a
hydroxy42 <farhan2262003@gmail.com>
2024-03-23 16:52:56 +0530

    htwd

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -14,7 +14,7 @@ export default function Page() {
         <div className="bg-black text-white py-1">
           <div className="container mx-auto flex flex-col md:flex-row items-center my-12 md:my-24">
             <div className="flex flex-col w-full lg:w-1/3 justify-center items-start p-4">
-            <Image src="/NexusLogo.png" alt="TechFest" className="w-[50vh] h-[50vh]" />
+            <Image src="/NexusLogo.png" alt="TechFest" className="w-[50vh] h-[50vh]"width={700} height={700} />
 
               <p className=" text-3xl md:text-base text-gray-50 mb-4">Explore your favorite events and register now to showcase your talent and win exciting prizes.</p>
               
@@ -29,7 +29,7 @@ export default function Page() {
                 </div>
                 <div>
                   <Image className="inline-block mt-28 hidden lg:block" src="https://user-images.githubusercontent.com/54521023/116969939-c1d5f180-acd4-11eb-8ad4-9ab9143bdb50.png" alt="Image 3" /> */}
-                  <Image src="/XYZ.gif" className='w-full h-[94%] ml-4' alt="" />
+                  <Image src="/XYZ.gif" className='w-full h-[94%] ml-4' width={800} height={500} alt="" />
                 </div>
                 
               </div>
