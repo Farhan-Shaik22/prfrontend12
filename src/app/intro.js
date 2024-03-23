b8c80ab183d713cce8bb162bd86f22be7349d290
vardaanaroracodes <vardaanbhatia.perso@gmail.com>
2024-03-23 10:52:11 +0530

    done with the cards,final

--- src/app/intro.js
+++ src/app/intro.js
@@ -5,7 +5,8 @@ const duction = {
         image:"/Vachan.jpeg",
         name: "Vachan 1",
         quote: "\" Debate: Polish your argumentative skills as you debate your opponent and prove your point to take home a prize.  \"",
-
+        par:"Team of 4",
+        reg:"INR 75",
        
         insta:"https://www.instagram.com/vachan.official/"
       },
@@ -15,8 +16,8 @@ const duction = {
         image:"/Vachan.jpeg",
         name: "Vachan 2",
         quote: "\" Declamation: Take command by holding the attention of the audience and moving them with your speech. \"",
-
-       
+        par:"Solo",
+        reg:"INR 75",
         insta:"https://www.instagram.com/vachan.official/"
       },
 
@@ -25,7 +26,8 @@ const duction = {
         image:"/Vachan.jpeg",
         name: "Vachan 3",
         quote: "\" Commentary contest: Test your skill to make scenarios better with quick responses as you commentate and prove your mettle. \"",
-
+        par:"Team of 2",
+        reg:"INR 75",
        
         insta:"https://www.instagram.com/vachan.official/"
       },
@@ -38,6 +40,8 @@ const duction = {
         image:"https://scontent.cdninstagram.com/v/t51.2885-19/258731026_459779118901767_1434310398993580285_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent.cdninstagram.com&_nc_cat=103&_nc_ohc=EuN9XHtBwJkAX_bmxqT&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfAjaUnzrNfiTyGJEJxtpmrbIX8aFgjOri6KDG_GW08GXA&oe=660215D8&_nc_sid=10d13b",
         name: "Aalap 1",
         quote: "\" Solo Melodies : Calling all music Enthusiasts!! Here’s your chance to grab the mic and show off your harmonies in this solo singing competition!  \"",
+        par:"Solo",
+        reg:"INR 100",
         insta:"https://www.instagram.com/aalapofficial/"
       },
 
@@ -46,6 +50,8 @@ const duction = {
         image:"https://scontent.cdninstagram.com/v/t51.2885-19/258731026_459779118901767_1434310398993580285_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent.cdninstagram.com&_nc_cat=103&_nc_ohc=EuN9XHtBwJkAX_bmxqT&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfAjaUnzrNfiTyGJEJxtpmrbIX8aFgjOri6KDG_GW08GXA&oe=660215D8&_nc_sid=10d13b",
         name: "Aalap 2",
         quote: "\" Melody Mania!: Get ready for the ultimate battle of bands! Our stage awaits a stellar musical performance by your team. \"",
+        par:"Team(3-5)",
+        reg:"INR 350",
         insta:"https://www.instagram.com/aalapofficial/"
       },
 
@@ -55,6 +61,8 @@ const duction = {
         image:"https://scontent.cdninstagram.com/v/t51.2885-19/412046551_359182106802041_6323071453295358439_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent.cdninstagram.com&_nc_cat=105&_nc_ohc=67MBY7Le4D0AX-9bOGD&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfA-3gaThnqQx28ZJ02QPPn6jzdbk9Szav86M_CJuizSbQ&oe=660131FF&_nc_sid=10d13b",
         name: "Mudra 1",
         quote: "\" Classical - It’s time to steal the spotlight under the classical hymns and mesmerize the audience with your graceful performance. \"",
+        par:"Solo",
+        reg:"INR 100",
         insta:"https://www.instagram.com/mudraofficial/"
       },
 
@@ -63,6 +71,8 @@ const duction = {
         image:"https://scontent.cdninstagram.com/v/t51.2885-19/412046551_359182106802041_6323071453295358439_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent.cdninstagram.com&_nc_cat=105&_nc_ohc=67MBY7Le4D0AX-9bOGD&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfA-3gaThnqQx28ZJ02QPPn6jzdbk9Szav86M_CJuizSbQ&oe=660131FF&_nc_sid=10d13b",
         name: "Mudra 2",
         quote: "\" Freestyle Competition(Group) - Showcase your best moves in this freestyle dance competition and groove your way to victory! \"",
+        par:"Team of 5",
+        reg:"INR 350",
         insta:"https://www.instagram.com/mudraofficial/"
       },
 
@@ -71,6 +81,8 @@ const duction = {
         image:"https://scontent.cdninstagram.com/v/t51.2885-19/412046551_359182106802041_6323071453295358439_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent.cdninstagram.com&_nc_cat=105&_nc_ohc=67MBY7Le4D0AX-9bOGD&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfA-3gaThnqQx28ZJ02QPPn6jzdbk9Szav86M_CJuizSbQ&oe=660131FF&_nc_sid=10d13b",
         name: "Mudra 3",
         quote: "\" Freestyle Competition(Solo) - Showcase your best moves in this freestyle dance competition and groove your way to victory! \"",
+        par:"Solo",
+        reg:"INR 100",
         insta:"https://www.instagram.com/mudraofficial/"
       },
 
@@ -79,6 +91,8 @@ const duction = {
         image:"https://scontent.cdninstagram.com/v/t51.2885-19/231585681_831325504186531_5797848488481493219_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent.cdninstagram.com&_nc_cat=107&_nc_ohc=r50hr0lRy0gAX_CDIzC&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfA4GA-z6GF315n4tRp70swD2VAQaxMzoJ7TZJ5uGCbAJw&oe=660195A8&_nc_sid=10d13b",
         name: "Aakarshan",
         quote: "\"Spin the wheel: Let your art do the talking in this game of chance and skill by spinning the wheel and creating a masterpiece to secure the price. \" ",
+        par:"Solo",
+        reg:"INR 75",
         insta:"https://www.instagram.com/aakarshanofficial/"
       },
 
@@ -88,6 +102,8 @@ const duction = {
         image:"https://scontent.cdninstagram.com/v/t51.2885-19/324910135_5956884101001489_5671979558690848789_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent.cdninstagram.com&_nc_cat=109&_nc_ohc=5TnSGkmWrwEAX8oaiNB&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfC0ZObtYShTiApjzth7Rlg0VkGzy8ShonepaKD7kLWF7g&oe=66027CA9&_nc_sid=10d13b",
         name: "Riti",
         quote: "\" Ludo: Creativity meets fun in a perfect fusion of upcycling, fashion and nostalgia on a life-sized Ludo board. Participate to win exciting cash prizes! \"",
+        par:"Team of 4",
+        reg:"INR 50",
         linkedin: "https://www.linkedin.com/in/dheeraj-koka-692a81291/",
         insta:"https://www.instagram.com/riti._.official/"
       },
@@ -97,6 +113,8 @@ const duction = {
         image:"https://scontent.cdninstagram.com/v/t51.2885-19/229604920_1493363987688976_8376050562174167657_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent.cdninstagram.com&_nc_cat=104&_nc_ohc=PBhwDa9zWawAX8cT0zo&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfCICW9rj0KwiRepsp9o7DgkFF3XCn2y5tO3-drfzyizCg&oe=6601A8A3&_nc_sid=10d13b",
         name: "Recurse 1",
         quote: "\" Gameathon: Turn the gears of your brain as you brainstorm ideas with your teammates in the ideathon and nurture your innovative potential/flaunt your presentation skills. \"",
+        par:"Team of 3",
+        reg:"INR 150",
         insta:"https://www.instagram.com/recurse.official/"
       },
 
@@ -105,48 +123,72 @@ const duction = {
         image:"https://scontent.cdninstagram.com/v/t51.2885-19/229604920_1493363987688976_8376050562174167657_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent.cdninstagram.com&_nc_cat=104&_nc_ohc=PBhwDa9zWawAX8cT0zo&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfCICW9rj0KwiRepsp9o7DgkFF3XCn2y5tO3-drfzyizCg&oe=6601A8A3&_nc_sid=10d13b",
         name: "Recurse 2",
         quote: "\" KBC: Navigate through OG trivia rounds such as fastest finger first, Audience poll etc as you exhibit your gaming and technical knowledge. \"",
+        par:"Solo",
+        reg:"INR 50",
         insta:"https://www.instagram.com/recurse.official/"
+        
       },
 
       {
         id: "13",
-        image:"",
+        image:"https://scontent.cdninstagram.com/v/t51.2885-19/39763914_343792263052808_3984688769658781696_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent.cdninstagram.com&_nc_cat=108&_nc_ohc=k9mptA7Od5EAX8ceEWL&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfCgc1GUngbQyxIXnKHnuzenHQUs96OGILBNA1z68v542w&oe=66038F37&_nc_sid=10d13b",
         name: "Traces Of Lenses",
         quote: "\" 1)Capture Quest: Take part in this thrilling photography competition, capturing unique visions through fun challenges and honing your skills! \"",
+        par:"Solo",
+        reg:"INR 100",
         insta:"https://www.instagram.com/tracesoflenses/"
       },
 
       {
         id: "14",
-        image:"",
+        image:"https://scontent.cdninstagram.com/v/t51.2885-19/285363858_998732150843465_1089754243808318901_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent.cdninstagram.com&_nc_cat=105&_nc_ohc=dPhbP-GbSqMAX8M397f&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfAt9bCpyuNE_c8RhoCrOw6gXMw0jp_uu-zvgRrrtcncmQ&oe=66041B36&_nc_sid=10d13b",
         name: "Kaivalya",
         quote: "\" Yoga Competition: Embrace calmness amid chaos, and exhibit your sense of balance and control over your body through fun yoga asanas.  \"",
+        par:"Solo",
+        reg:"INR 50",
         insta:"https://www.instagram.com/kaivalya_official/"
       },
 
       {
         id: "15",
-        image:"/esports.png",
-        name: "E-Sports 1",
-        quote: "\"  Valorant: pump up your adrenaline as you battle to outsmart other teams using abilities, precision and Maneuvers to come out on top.   \" ",
+        image:"https://scontent.cdninstagram.com/v/t51.2885-19/65730805_964871123844995_157515602003492864_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent.cdninstagram.com&_nc_cat=111&_nc_ohc=lRzVK1cZ6sAAX9EoyEG&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfBdI9VmifBxR2-_5TSh_qj6grJCWX-dm6zspihaphJ2Tg&oe=6603FAF0&_nc_sid=10d13b",
+        name: "Abhinaya",
+        quote: "\"Rangasthalam: Channel your inner Rajamouli/cinematic genius by engaging in a script writing and acting competition where you display your cinematic expertise.  \"",
+        par:"Team of 4",
+        reg:"INR 200",
         insta:"https://www.instagram.com/kaivalya_official/"
       },
 
       {
         id: "16",
         image:"/esports.png",
-        name: "E-Sports 2",
-        quote: "\"  FIFA: Dribble through the field as you crush the defenders to score goals and dominate your opponent to be a champion.  \"",
+        name: "E-Sports 1",
+        quote: "\"  Valorant: pump up your adrenaline as you battle to outsmart other teams using abilities, precision and Maneuvers to come out on top.   \" ",
+        par:"Team of 5",
+        reg:"INR 500",
         insta:"https://www.instagram.com/kaivalya_official/"
       },
 
       {
         id: "17",
         image:"/esports.png",
+        name: "E-Sports 2",
+        quote: "\"  FIFA: Dribble through the field as you crush the defenders to score goals and dominate your opponent to be a champion.  \"",
+        par:"Solo",
+        reg:"INR 100",
+        insta:"https://www.instagram.com/kaivalya_official/"
+      },
+
+      {
+        id: "18",
+        image:"/esports.png",
         name: "E-Sports 3",
         quote: "\"  BGMI: Work with your squad to secure a spot in the leaderboard by healing, flanking, camping and defeating your enemies.  \"",
+        par:"Team of 4",
+        reg:"INR 200",
         insta:"https://www.instagram.com/kaivalya_official/"
       }
+      
       
     ]
 
