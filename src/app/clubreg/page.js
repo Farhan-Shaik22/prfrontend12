408956847064354194beea2c57658e2f0755c900
hydroxy42 <farhan2262003@gmail.com>
2024-03-24 23:28:47 +0530

    bugs fix

--- src/app/clubreg/page.js
+++ src/app/clubreg/page.js
@@ -15,6 +15,7 @@ export default function ClubTransaction() {
   const [token, setToken] = useState('');
   const [id, setId] = useState(null);
   const [registrationCount, setRegistrationCount] = useState(0);
+  const [isRegistered, setIsRegistered] = useState(false); // New state to track registration status
   const [inputDisabled, setInputDisabled] = useState(false);
 
   useEffect(() => {
@@ -22,7 +23,7 @@ export default function ClubTransaction() {
     const idFromSearchParams = searchParams.get('id');
     setId(idFromSearchParams);
   }, []);
-  
+
   const clubs = [
     "Vachan 1",
     "Vachan 2",
@@ -42,12 +43,9 @@ export default function ClubTransaction() {
     "E-Sports 1",
     "E-Sports 2",
     "E-Sports 3"
-  ]
-  ;
-  const caps =[50, 50, 50, 100, 75, 100, 75, 75, 1000, 24, 1000, 1000, 1000, 1000, 1000, 32, 32, 32]
+  ];
 
-
-
+  const caps = [50, 50, 50, 100, 75, 100, 75, 75, 1000, 24, 1000, 1000, 1000, 1000, 1000, 32, 32, 32];
 
   useEffect(() => {
     const storedToken = localStorage.getItem('token');
@@ -64,7 +62,7 @@ export default function ClubTransaction() {
         .then(response => {
           if (response.data.success) {
             setRegistrationCount(response.data.count);
-            setInputDisabled(response.data.count >= caps[clubs.indexOf(duction.all.find(profile => profile.id === id)?.name)]); // Disable input if count >= 1
+            setInputDisabled(response.data.count >= caps[clubs.indexOf(duction.all.find(profile => profile.id === id)?.name)]);
           } else {
             console.error('Error getting club registration count:', response.data.message);
             setInputDisabled(true);
@@ -73,10 +71,30 @@ export default function ClubTransaction() {
         .catch(error => {
           console.error('Error getting club registration count:', error);
           setInputDisabled(true);
-          // console.log(duction.all.find(profile => profile.id === id)?.name);
         });
     }
   }, [id]);
+
+  useEffect(() => {
+    if (id && rollNumber) {
+      axios.get(`https://backend-fypg.onrender.com/api/clubs/${rollNumber}`)
+        .then(response => {// Log response data
+          if (response.data.success) {
+            const { clubs } = response.data; // Extract clubs array from response.data
+            
+            // Check if clubs array is defined and includes the club with the specified ID
+            if (clubs && clubs.includes(duction.all.find(profile => profile.id === id)?.name)) {
+              setIsRegistered(true);
+            }
+          }
+        })
+        .catch(error => {
+          console.error('Error checking registration status:', error);
+        });
+    }
+  }, [id, rollNumber]);
+  
+  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -90,20 +108,14 @@ export default function ClubTransaction() {
           },
         }
       );
-      // console.log('Transaction submitted:', response.data);
-      // Show success message
       alert('Transaction submitted successfully!');
-      // Redirect back to clubs page after 2 seconds
       setTimeout(() => {
-        window.location.href = 'https://linktr.ee/Public_relations'; // Assuming the route to clubs page is '/clubs'
+        window.location.href = 'https://linktr.ee/Public_relations'; 
       }, 2000);
     } catch (error) {
-      console.error('Error submitting transaction:', error.response.data);
-      // Show error message if submission fails
-      alert('Error submitting transaction. Please try again later.');
+      alert(error.response.data.message);
     }
   };
-  
 
   return (
     <>
@@ -111,10 +123,8 @@ export default function ClubTransaction() {
       {rollNumber ? (
         <div>
           <div className='flex justify-end mt-8'>
-          {/* <Image src="/pac1.gif" className='w-[40%] h-[40%] mr-40' alt="" /> */}
-          <img src="/pac1.gif" className='w-[40%] h-[40%] mr-40' alt="" />
+            <img src="/pac1.gif" className='w-[40%] h-[40%] mr-40' alt="" />
             <div className='w-[40%]'>
-              
               <Typography className="animate-pulse mt-5 text-center text-5xl font-extrabold font-pixel text-gray-400">
                 Last step to Nexus
               </Typography>
@@ -126,35 +136,41 @@ export default function ClubTransaction() {
                   </Typography>
                 ))
               }
-              {registrationCount >= caps[clubs.indexOf(duction.all.find(profile => profile.id === id)?.name)] || inputDisabled ? (
+              {isRegistered ? ( // Check if the user is already registered to disable input and remove QR
+                <Typography variant="h6" color="white" className="text-2xl text-center mt-8 ml-4">
+                  Already Registered
+                </Typography>
+              ) : registrationCount >= caps[clubs.indexOf(duction.all.find(profile => profile.id === id)?.name)] || inputDisabled ? (
                 <Typography variant="h6" color="white" className="text-2xl text-center mt-8 ml-4">
                   Registrations Closed
                 </Typography>
               ) : (
                 <Image
-                  src={ duction.all.find(profile => profile.id === id)?.qr}// Route of the image file
-                  height={350} // Desired size in pixels
-                  width={350} // Desired size in pixels
+                  src={duction.all.find(profile => profile.id === id)?.qr}
+                  height={350} 
+                  width={350} 
                   className='mt-5 ml-[20%]'
                   alt="Your Image"
                 />
               )}
-              <form onSubmit={handleSubmit} className="mt-8 space-y-8">
-                <Typography variant="h6" color="white" className="text-2xl">
-                  Add your Transaction ID
-                </Typography>
-                <Input
-                  type="text"
-                  placeholder="Transaction ID"
-                  value={transactionId}
-                  onChange={(e) => setTransactionId(e.target.value)}
-                  className="text-white placeholder-gray-600 !border-t-blue-gray-200 focus:!border-blue-700 h-14"
-                  disabled={inputDisabled} // Disable input based on state
-                />
-                <Button type="submit" fullWidth className="bg-green-300 text-white font-bold text-2xl font-pixel h-14" disabled={inputDisabled}>
-                  Submit
-                </Button>
-              </form>
+              {!isRegistered && (
+                <form onSubmit={handleSubmit} className="mt-8 space-y-8">
+                  <Typography variant="h6" color="white" className="text-2xl">
+                    Add your Transaction ID
+                  </Typography>
+                  <Input
+                    type="text"
+                    placeholder="Transaction ID"
+                    value={transactionId}
+                    onChange={(e) => setTransactionId(e.target.value)}
+                    className="text-white placeholder-gray-600 !border-t-blue-gray-200 focus:!border-blue-700 h-14"
+                    disabled={inputDisabled || isRegistered} 
+                  />
+                  <Button type="submit" fullWidth className="bg-green-300 text-white font-bold text-2xl font-pixel h-14" disabled={inputDisabled || isRegistered}>
+                    Submit
+                  </Button>
+                </form>
+              )}
             </div>
           </div>
         </div>
